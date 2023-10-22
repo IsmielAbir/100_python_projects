3bce6109844fcfe0402dfd917dd47f4fb0577120
Ismielabir <ismielabir286@gmail.com>
2023-10-23 00:29:12 +0600

    first commit

---
+++ 1_Search_and_Rescue_game/bayes.py
@@ -0,0 +1,16 @@
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
+map_pic = 'cp.png'
+
+
+c1 = (130,265,180,315)
+c2 = (80,255,130,305)
+c3 = (105,205,155,255)
+
+
+class Search():
+    pass
